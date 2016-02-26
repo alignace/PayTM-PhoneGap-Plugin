50b23046203ddb33166ada887784810ceea3172b
Ayajahmed Shaikh <ayajshaikh@users.noreply.github.com>
2016-02-26 18:25:42 +0530

    Updated to new paytm ios sdk library

--- src/ios/PayTMCordova.m
+++ src/ios/PayTMCordova.m
@@ -25,23 +25,51 @@
     NSString* paytm_ind_type_id = [mainBundle objectForInfoDictionaryKey:@"PayTMIndustryTypeID"];
     NSString* paytm_website = [mainBundle objectForInfoDictionaryKey:@"PayTMWebsite"];
     
+    //Step 1: Create a default merchant config object
     PGMerchantConfiguration* merchant = [PGMerchantConfiguration defaultConfiguration];
-    merchant.merchantID = paytm_merchant_id;
-    merchant.industryID = paytm_ind_type_id;
-    merchant.website = paytm_website;
-    merchant.channelID = @"WAP";
     
+    //Step 2: If you have your own checksum generation and validation url set this here. Otherwise use the default Paytm urls
     merchant.checksumGenerationURL = paytm_generate_url;
     merchant.checksumValidationURL = paytm_validate_url;
     
-    PGOrder* order =
-    [PGOrder orderForOrderID:orderId customerID:customerId amount:amount customerMail:email customerMobile:phone];
+    //Step 3: Create the order with whatever params you want to add. But make sure that you include the merchant mandatory params
+    NSMutableDictionary *orderDict = [NSMutableDictionary new];
+    //Merchant configuration in the order object
+    //orderDict[@"REQUEST_TYPE"] = @"DEFAULT";
+    orderDict[@"CHANNEL_ID"] = @"WAP";
+    orderDict[@"THEME"] = @"merchant";
+    orderDict[@"INDUSTRY_TYPE_ID"] = paytm_ind_type_id;
+    orderDict[@"MID"] = paytm_merchant_id;
+    orderDict[@"WEBSITE"] = paytm_website;
     
+    //Order configuration in the order object
+    orderDict[@"TXN_AMOUNT"] = amount;
+    orderDict[@"ORDER_ID"] = orderId;
+    orderDict[@"CUST_ID"] = customerId;
+    orderDict[@"EMAIL"] = email;
+    orderDict[@"MOBILE_NO"] = phone;
+    
+    PGOrder *order = [PGOrder orderWithParams:orderDict];
+    
+    //Step 4: Choose the PG server.
     txnController = [[PGTransactionViewController alloc] initTransactionForOrder:order];
     txnController.serverType = eServerTypeProduction;
     txnController.merchant = merchant;
     txnController.delegate = self;
-    txnController.loggingEnabled = true;
+    //txnController.loggingEnabled = true;
+    
+    UIView *headerView = [[UIView alloc]initWithFrame:CGRectMake(0.0f, 0.0f, 20.0f, 64.0f)];
+    headerView.backgroundColor = [UIColor clearColor];
+    UIView * topBar = [[UIView alloc]initWithFrame:CGRectMake(0.0f, 20.0f, 320.0f, 50.0f)];
+    [topBar setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"topBar"]]];
+    [headerView addSubview:topBar];
+    txnController.topBar = headerView;
+    
+    UIButton *cancelButton = [[UIButton alloc]initWithFrame:CGRectMake(10.0f, 25.0f, 60.0f, 40.0f)];
+    [cancelButton setBackgroundImage:[UIImage imageNamed:@"cancel"] forState:UIControlStateNormal];
+    txnController.cancelButton = cancelButton;
+    
+    
     UIViewController *rootVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 //    [rootVC.navigationController pushViewController:txnController animated:true];
     [rootVC presentViewController:txnController animated:YES completion:nil];
@@ -73,12 +101,5 @@
     [self.commandDelegate sendPluginResult:result callbackId:callbackId];
     [txnController dismissViewControllerAnimated:YES completion:nil];
 }
-
-//Called when CHeckSum HASH Generation completes either by PG_Server Or Merchant server.
-// - (void)didFinishCASTransaction:(PGTransactionViewController *)controller response:(NSDictionary *)response{
-//     CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:response];
-//     [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-//     [txnController dismissViewControllerAnimated:YES completion:nil];
-// }
 
 @end
